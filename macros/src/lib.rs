634122296031f23375382454122148874a82992a
Yasunori Ishiguro <xjr1300.04@gmail.com>
2024-03-25 09:40:27 +0900

    Divide the DomainPrimitive derive macro into PrimitiveDisplay.

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,12 +2,12 @@ use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
 
 mod domain_primitive;
-use domain_primitive::{impl_domain_primitive, impl_string_primitive};
+use domain_primitive::{impl_domain_primitive, impl_primitive_display, impl_string_primitive};
 mod types;
 
 /// `DomainPrimitive`導出マクロ
 ///
-/// `value`フィールドを持つ構造体に、`value`メソッドと`std::fmt::Display`を実装する。
+/// `value`フィールドを持つ構造体に`value`メソッドを実装する。
 ///
 /// 1. `value`フィールドが`Copy`トレイトを実装している型の場合、`#[value_getter(ret = "val")]`
 /// 2. `value`フィールドが`Copy`トレイトを実装していない型で、その参照を`value`メソッドが返す場合、`#[value_getter(ret = "ref")]`
@@ -19,6 +19,19 @@ pub fn derive_domain_primitive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
     match impl_domain_primitive(input) {
+        Ok(token_stream) => TokenStream::from(token_stream),
+        Err(err) => TokenStream::from(err.into_compile_error()),
+    }
+}
+
+/// `PrimitiveDisplay`導出マクロ
+///
+/// `value`フィールドを持つ構造体に`std::fmt::Display`を実装する。
+#[proc_macro_derive(PrimitiveDisplay)]
+pub fn derive_primitive_display(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    match impl_primitive_display(input) {
         Ok(token_stream) => TokenStream::from(token_stream),
         Err(err) => TokenStream::from(err.into_compile_error()),
     }
