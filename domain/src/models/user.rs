1afa793c255c781575b4424b71ed7f41512457d4
Yasunori Ishiguro <xjr1300.04@gmail.com>
2024-03-25 09:21:22 +0900

    Implement the EmailAddress domain primitive.

--- domain/src/models/user.rs
+++ domain/src/models/user.rs
@@ -1,36 +1,41 @@
 use validator::Validate;
 
-use macros::DomainPrimitive;
+use macros::{DomainPrimitive, StringPrimitive};
 
 use crate::common::error::{DomainError, DomainResult};
 
+/// ユーザー
+///
+/// ユーザーが保有するフィールドを次に示す。
+///
+/// * ユーザーID
+/// * Eメール・アドレス
+/// * パスワード
+/// * アクティブ・フラグ
+/// * 名前（姓）
+/// * 名前（名）
+/// * 郵便番号
+/// * 住所
+/// * 固定電話番号
+/// * 携帯電話番号
+/// * 備考
+/// * 参加日時
+/// * 作成日時
+/// * 更新日時
+///
+/// ユーザーは、Eメール・アドレスとパスワードで認証する。
+/// アクティブ・フラグが`true`のユーザーのみ認証できる。
+/// アプリケーション設定の大認証繰り返し時間内に、最大認証繰り返し回数より多く認証に失敗したとき、
+/// 認証に失敗させて、次回以降の認証をすべて拒否する。
+
 /// Eメール・アドレス
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Validate, DomainPrimitive)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Validate, DomainPrimitive, StringPrimitive)]
 pub struct EmailAddress {
     #[validate(email)]
     #[value_getter(ret = "ref", rty = "&str")]
     value: String,
 }
 
-impl EmailAddress {
-    /// Eメール・アドレスを構築する。
-    ///
-    /// # 引数
-    ///
-    /// * `email` - Eメール・アドレス
-    ///
-    /// # 戻り値
-    ///
-    /// Eメールアドレス
-    pub fn new<T: ToString>(email: T) -> DomainResult<Self> {
-        let instance = Self {
-            value: email.to_string(),
-        };
-        match instance.validate() {
-            Ok(_) => Ok(instance),
-            Err(_) => Err(DomainError::Validation("email address is invalid".into())),
-        }
-    }
 }
 
 #[cfg(test)]
