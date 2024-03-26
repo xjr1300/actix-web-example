09bfc649fa396fdb3585c7c3acf953cf3c5fd517
Yasunori Ishiguro <xjr1300.04@gmail.com>
2024-03-26 14:09:13 +0900

    Implement logging feature.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,27 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
+use server::telemetry::{generate_log_subscriber, init_log_subscriber};
+use tracing_actix_web::TracingLogger;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(health_check)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    // サブスクライバを初期化
+    let subscriber = generate_log_subscriber("actix_web_example".into(), "info");
+    init_log_subscriber(subscriber);
+
+    // HttpServerを起動
+    HttpServer::new(|| {
+        App::new()
+            .wrap(TracingLogger::default())
+            .route("/", web::get().to(health_check))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
 
 /// ヘルス・チェック
+#[tracing::instrument(name = "health check")]
 pub async fn health_check() -> impl Responder {
     HttpResponse::Ok().body("It works!")
 }
