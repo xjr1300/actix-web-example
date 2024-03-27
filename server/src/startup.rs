49f15c2cfe02f3e15c6956b649bd99df4d1a3ea5
Yasunori Ishiguro <xjr1300.04@gmail.com>
2024-03-27 19:09:05 +0900

    Implement the feature to connect to the database.

--- server/src/startup.rs
+++ server/src/startup.rs
@@ -2,6 +2,7 @@ use std::net::TcpListener;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 
 /// HTTPサーバーを構築する。
@@ -9,16 +10,18 @@ use tracing_actix_web::TracingLogger;
 /// # 引数
 ///
 /// * `listener` - HTTPサーバーがリッスンするポートをバインドしたリスナー
+/// * `pool` - データベース接続プール
 ///
 /// # 戻り値
 ///
 /// HTTPサーバー
-pub fn build_http_server(listener: TcpListener) -> anyhow::Result<Server> {
+pub fn build_http_server(listener: TcpListener, pool: PgPool) -> anyhow::Result<Server> {
     // HttpServerを構築
-    Ok(HttpServer::new(|| {
+    Ok(HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
+            .app_data(pool.clone())
     })
     .listen(listener)?
     .run())
--- test_suite/tests/helpers.rs
+++ test_suite/tests/helpers.rs
@@ -1,8 +1,14 @@
 use std::net::TcpListener;
+use std::path::Path;
 
 use anyhow::Context as _;
 use once_cell::sync::Lazy;
+use sqlx::{Connection as _, Executor as _, PgConnection, PgPool};
+use uuid::Uuid;
 
+use server::settings::{
+    retrieve_app_settings, AppEnvironment, DatabaseSettings, SETTINGS_DIR_NAME,
+};
 use server::startup::build_http_server;
 use server::telemetry::{generate_log_subscriber, init_log_subscriber};
 
@@ -32,16 +38,63 @@ pub struct TestApp {
 ///
 /// 統合テスト用のHTTPサーバーのルートURI
 pub async fn spawn_app_for_integration_test() -> anyhow::Result<TestApp> {
+    dotenvx::dotenv()?;
     Lazy::force(&TRACING);
+
+    // 環境変数からアプリケーションの動作環境を取得
+    let app_env: AppEnvironment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| String::from("development"))
+        .into();
+
+    // アプリケーション設定を取得
+    let dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let settings_dir = dir.join("..").join(SETTINGS_DIR_NAME);
+    let mut app_settings = retrieve_app_settings(app_env, settings_dir)?;
+
+    // テスト用のデータベースの名前を設定
+    app_settings.database.name = format!("test_awe_{}", Uuid::new_v4());
+    // テスト用のデータベースを作成して、接続及び構成
+    let pool = configure_database(&app_settings.database).await?;
 
     // 統合テストが終了すると、HTTPサーバーがリッスンするポートが閉じられる。
     // すると、actix-webが提供する`Server`が終了して、ここで生み出したスレッドが終了する。
     let listener = TcpListener::bind("localhost:0").context("failed to bind random port")?;
     let port = listener.local_addr().unwrap().port();
-    let server = build_http_server(listener)?;
+    let server = build_http_server(listener, pool)?;
     tokio::spawn(server);
 
     Ok(TestApp {
         root_uri: format!("http://localhost:{}", port),
     })
+}
+
+/// データベースを作成して、接続及び構成する。
+///
+/// # 引数
+///
+/// * `settings` - データベース設定
+///
+/// # 戻り値
+///
+/// データベース接続プール
+pub async fn configure_database(settings: &DatabaseSettings) -> anyhow::Result<PgPool> {
+    // データベースを構築
+    let mut connection = PgConnection::connect_with(&settings.without_db())
+        .await
+        .expect("Fail to connect to postgres.");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, settings.name).as_str())
+        .await
+        .expect("Failed to create test database.");
+
+    // データベースに接続
+    let pool = PgPool::connect_with(settings.with_db()).await?;
+    // データベースをマイグレート
+    let crate_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let migrations_dir = crate_dir.join("..").join("migrations");
+    if migrations_dir.is_dir() {
+        sqlx::migrate!("../migrations").run(&pool).await?;
+    }
+
+    Ok(pool)
 }
