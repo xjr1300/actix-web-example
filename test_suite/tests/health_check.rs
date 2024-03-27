c18914c185745bc052feb4b611de55da10793f67
Yasunori Ishiguro <xjr1300.04@gmail.com>
2024-03-27 09:48:52 +0900

    Switch log outputting for integration test.

--- test_suite/tests/health_check.rs
+++ test_suite/tests/health_check.rs
@@ -1,18 +1,40 @@
 use std::net::TcpListener;
 
 use anyhow::Context as _;
+use once_cell::sync::Lazy;
+
+use server::telemetry::{generate_log_subscriber, init_log_subscriber};
+
+/// 統合テスト用アプリ
+pub struct TestApp {
+    /// アプリのルートURI
+    pub root_uri: String,
+}
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_level = log::Level::Info;
+    let subscriber_name = String::from("test");
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = generate_log_subscriber(subscriber_name, default_level, std::io::stdout);
+        init_log_subscriber(subscriber);
+    } else {
+        let subscriber = generate_log_subscriber(subscriber_name, default_level, std::io::sink);
+        init_log_subscriber(subscriber);
+    }
+});
 
 /// ヘルス・チェック・ハンドラ
 #[tokio::test]
 #[ignore]
 async fn health_check_works() -> anyhow::Result<()> {
     // 準備
-    let address = spawn_http_server().await?;
+    let app = spawn_app_for_integration_test().await?;
     let client = reqwest::Client::new();
 
     // 実行
     let response = client
-        .get(&format!("{address}/health_check"))
+        .get(&format!("{}/health_check", app.root_uri))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -29,11 +51,15 @@ async fn health_check_works() -> anyhow::Result<()> {
 /// # 戻り値
 ///
 /// 統合テスト用のHTTPサーバーのルートURI
-async fn spawn_http_server() -> anyhow::Result<String> {
+async fn spawn_app_for_integration_test() -> anyhow::Result<TestApp> {
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("localhost:0").context("failed to bind random port")?;
     let port = listener.local_addr().unwrap().port();
     let server = server::startup::build_http_server(listener)?;
     tokio::spawn(server);
 
-    Ok(format!("http://localhost:{}", port))
+    Ok(TestApp {
+        root_uri: format!("http://localhost:{}", port),
+    })
 }
