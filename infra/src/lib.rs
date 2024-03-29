da3dcb8c565ba3eba29d0ebecc0ae28758f08fb0
Yasunori Ishiguro <xjr1300.04@gmail.com>
2024-03-29 19:22:16 +0900

    Implement the option_primitive declarative  macro.

--- infra/src/lib.rs
+++ infra/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use paste::paste;
+
+use domain::models::primitives::{FixedPhoneNumber, MobilePhoneNumber, Remarks};
+
+pub mod repositories;
+
+macro_rules! optional_primitive {
+    ($t1:ty, $t2:ty) => {
+        paste! {
+            pub(crate) fn [<optional_ $t1:snake:lower _primitive>](value: Option<$t2>) -> Option<$t1> {
+                match value {
+                    Some(value) => Some($t1::new(value).unwrap()),
+                    None => None,
+                }
+            }
+
+            #[allow(dead_code)]
+            pub(crate) fn [<optional_ $t1:snake:lower _value>](instance: &Option<$t1>) -> Option<$t2> {
+                match instance {
+                    Some(instance) => Some(instance.value().to_owned()),
+                    None => None,
+                }
+            }
+        }
+    };
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+optional_primitive!(FixedPhoneNumber, String);
+optional_primitive!(MobilePhoneNumber, String);
+optional_primitive!(Remarks, String);
