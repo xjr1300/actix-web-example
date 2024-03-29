b56c8e0af4f620745381d41b4a636b1eb86263d3
Yasunori Ishiguro <xjr1300.04@gmail.com>
2024-03-29 17:01:19 +0900

    Create the migration file for creating the users table.

---
+++ migrations/20240329073744_create_users_table.up.sql
@@ -0,0 +1,19 @@
+-- ユーザー・テーブルを作成
+CREATE TABLE IF NOT EXISTS users (
+    id UUID NOT NULL,
+    email VARCHAR(254) NOT NULL,
+    password VARCHAR(256) NOT NULL,
+    active BOOLEAN NOT NULL,
+    family_name VARCHAR(40) NOT NULL,
+    given_name VARCHAR(40) NOT NULL,
+    postal_code VARCHAR(8) NOT NULL,
+    address VARCHAR(80) NOT NULL,
+    fixed_phone_number VARCHAR(12),
+    mobile_phone_number VARCHAR(13),
+    remarks VARCHAR(400),
+    last_logged_in_at TIMESTAMP WITH TIME ZONE,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    CONSTRAINT pk_users PRIMARY KEY (id),
+    CONSTRAINT ak_users_email UNIQUE (email)
+);
