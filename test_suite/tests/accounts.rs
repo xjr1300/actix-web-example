d0c6f4ba44e8f6be58bd29410127a3b8b2bf53e3
Yasunori Ishiguro <xjr1300.04@gmail.com>
2024-04-04 15:23:52 +0900

    Implement test for signing up with invalid user permission code.

--- test_suite/tests/accounts.rs
+++ test_suite/tests/accounts.rs
@@ -4,20 +4,20 @@ use infra::routes::accounts::{SignUpReqBody, SignUpResBody};
 use infra::routes::ErrorResponseBody;
 use use_cases::UseCaseErrorCode;
 
-use crate::helpers::{signup_request_body_json, spawn_test_app, CONTENT_TYPE_APPLICATION_JSON};
+use crate::helpers::{sign_up_request_body_json, spawn_test_app, CONTENT_TYPE_APPLICATION_JSON};
 
 /// 妥当なユーザー情報で、ユーザーがサインアップできることを確認
 #[tokio::test]
 #[ignore]
-async fn user_can_signup_with_the_valid_info() -> anyhow::Result<()> {
+async fn user_can_sign_up_with_the_valid_info() -> anyhow::Result<()> {
     // 準備
     let app = spawn_test_app().await?;
 
-    let json_body = signup_request_body_json();
+    let json_body = sign_up_request_body_json();
     let body: SignUpReqBody = serde_json::from_str(&json_body).unwrap();
 
     // 実行
-    let response = app.request_accounts_signup(json_body).await?;
+    let response = app.request_accounts_sign_up(json_body).await?;
     let status_code = response.status();
     let headers = response.headers().clone();
     let content_type = headers.get(CONTENT_TYPE);
@@ -36,15 +36,15 @@ async fn user_can_signup_with_the_valid_info() -> anyhow::Result<()> {
 /// Eメールアドレスがすでに登録されている場合に、ユーザーがサインアップできないことを確認
 #[tokio::test]
 #[ignore]
-async fn user_can_not_signup_because_another_user_has_same_email_was_registered(
+async fn user_can_not_sign_up_because_another_user_has_same_email_was_registered(
 ) -> anyhow::Result<()> {
     // 準備
     let app = spawn_test_app().await?;
-    let json_body = signup_request_body_json();
+    let json_body = sign_up_request_body_json();
 
     // 実行
-    let _ = app.request_accounts_signup(json_body.clone()).await?;
-    let response = app.request_accounts_signup(json_body).await?;
+    let _ = app.request_accounts_sign_up(json_body.clone()).await?;
+    let response = app.request_accounts_sign_up(json_body).await?;
     let status_code = response.status();
     let headers = response.headers().clone();
     let content_type = headers.get(CONTENT_TYPE);
@@ -71,12 +71,13 @@ async fn user_can_not_signup_because_another_user_has_same_email_was_registered(
 /// 間違っている場合に、ユーザーがサインアップできないことを確認
 #[tokio::test]
 #[ignore]
-async fn user_can_not_signup_with_invalid_email() -> anyhow::Result<()> {
+async fn user_can_not_sign_up_with_invalid_email() -> anyhow::Result<()> {
     // 準備
     let app = spawn_test_app().await?;
-    let json_body = signup_request_body_json().replace("foo@example.com", "foo");
+    let json_body = sign_up_request_body_json().replace("foo@example.com", "foo");
 
-    let response = app.request_accounts_signup(json_body).await?;
+    // 実行
+    let response = app.request_accounts_sign_up(json_body).await?;
     let status_code = response.status();
     let headers = response.headers().clone();
     let content_type = headers.get(CONTENT_TYPE);
@@ -90,8 +91,10 @@ async fn user_can_not_signup_with_invalid_email() -> anyhow::Result<()> {
     );
     let content_type = content_type.unwrap().to_str()?;
     assert_eq!(CONTENT_TYPE_APPLICATION_JSON, content_type);
-    assert!(response_body.error_code.is_some());
-
+    assert_eq!(
+        Some(UseCaseErrorCode::Validation as u32),
+        response_body.error_code
+    );
     assert_eq!(
         "Eメール・アドレスの形式が間違っています。",
         response_body.message
@@ -103,15 +106,15 @@ async fn user_can_not_signup_with_invalid_email() -> anyhow::Result<()> {
 /// 固定電話番号と携帯電話番号が設定されていない場合に、ユーザーがサインアップできないことを確認
 #[tokio::test]
 #[ignore]
-async fn user_can_not_signup_without_fixed_phone_number_and_mobile_phone_number(
+async fn user_can_not_sign_up_without_fixed_phone_number_and_mobile_phone_number(
 ) -> anyhow::Result<()> {
     // 準備
     let app = spawn_test_app().await?;
-    let json_body = signup_request_body_json()
+    let json_body = sign_up_request_body_json()
         .replace(r#""099-472-1111""#, "null")
         .replace(r#""090-1234-5678""#, "null");
 
-    let response = app.request_accounts_signup(json_body).await?;
+    let response = app.request_accounts_sign_up(json_body).await?;
     let status_code = response.status();
     let headers = response.headers().clone();
     let content_type = headers.get(CONTENT_TYPE);
@@ -128,6 +131,42 @@ async fn user_can_not_signup_without_fixed_phone_number_and_mobile_phone_number(
     );
     assert_eq!(
         "ユーザーは固定電話番号または携帯電話番号を指定する必要があります。",
+        response_body.message
+    );
+
+    Ok(())
+}
+
+/// 妥当でないユーザー権限コードが設定されている場合に、ユーザーがサインアップできないことを確認
+#[tokio::test]
+#[ignore]
+async fn user_can_not_sign_up_when_user_permission_code_is_invalid() -> anyhow::Result<()> {
+    // 準備
+    let app = spawn_test_app().await?;
+    let json_body = sign_up_request_body_json()
+        .replace(r#""userPermissionCode": 1,"#, r#""userPermissionCode": 0,"#);
+
+    // 実行
+    let response = app.request_accounts_sign_up(json_body).await?;
+    let status_code = response.status();
+    let headers = response.headers().clone();
+    let content_type = headers.get(CONTENT_TYPE);
+    let response_body = response.json::<ErrorResponseBody>().await?;
+
+    // 検証
+    assert_eq!(reqwest::StatusCode::BAD_REQUEST, status_code);
+    assert_eq!(
+        Some(UseCaseErrorCode::Validation as u32),
+        response_body.error_code
+    );
+    let content_type = content_type.unwrap().to_str()?;
+    assert_eq!(CONTENT_TYPE_APPLICATION_JSON, content_type);
+    assert_eq!(
+        Some(UseCaseErrorCode::Validation as u32),
+        response_body.error_code
+    );
+    assert_eq!(
+        "ユーザー権限区分コードが範囲外です。",
         response_body.message
     );
 
